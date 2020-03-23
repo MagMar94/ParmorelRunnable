6dd8d5851c5e9974a3eec5727cc5dcf31c2bdf53
Magnus Marthinsen <magmar@online.no>
2020-03-23 16:56:57 +0100

    Fixed logging to file stored together with the repaired model

--- src/hvl/projectparmorel/modules/Experiment.java
+++ src/hvl/projectparmorel/modules/Experiment.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import hvl.projectparmorel.ecore.EcoreQModelFixer;
 import hvl.projectparmorel.general.ModelFixer;
@@ -12,17 +15,17 @@ import hvl.projectparmorel.modelrepair.Solution;
 import hvl.projectparmorel.utils.ParmorelUtils;
 
 public abstract class Experiment {
-	
+
 	private String fixedModelFolderName;
-	
+
 	public Experiment(String fixedModelFolderName) {
 		this.fixedModelFolderName = fixedModelFolderName + "/bestWeight";
 		File experimentResultFolder = new File(this.fixedModelFolderName);
-		if(!experimentResultFolder.exists()) {
+		if (!experimentResultFolder.exists()) {
 			experimentResultFolder.mkdir();
 		}
 	}
-	
+
 	/**
 	 * Repair the brokenModels with user settings.
 	 * 
@@ -46,12 +49,24 @@ public abstract class Experiment {
 	 */
 	public void repairModels(File[] brokenModels, int usersettings) {
 		ModelFixer ql = new EcoreQModelFixer(ParmorelUtils.generateUserSettings(usersettings));
+		Logger logger = Logger.getLogger("MyLog");
+		FileHandler fh = null;
 
 		for (int i = 0; i < brokenModels.length; i++) {
 			String iterationSpecificFolderName = fixedModelFolderName + "/model" + i;
 			File fixedModelFolder = new File(iterationSpecificFolderName);
 			if (!fixedModelFolder.exists()) {
 				fixedModelFolder.mkdir();
+			}
+			try {
+				fh = new FileHandler(iterationSpecificFolderName + "/LogFile.log");
+				logger.addHandler(fh);
+				SimpleFormatter formatter = new SimpleFormatter();  
+		        fh.setFormatter(formatter); 
+			} catch (SecurityException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
 			}
 
 			ql.fixModel(brokenModels[i]);
@@ -64,20 +79,28 @@ public abstract class Experiment {
 			File fixedModelFile = new File(iterationSpecificFolderName + "/" + brokenModels[i].getName());
 			try {
 				Files.copy(solution.getModel().toPath(), fixedModelFile.toPath());
-				Files.copy(knowledgeFile.toPath(), new File(iterationSpecificFolderName + "/" + knowledgeFile.getName()).toPath());
+				Files.copy(knowledgeFile.toPath(),
+						new File(iterationSpecificFolderName + "/" + knowledgeFile.getName()).toPath());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			
+			if (fh != null) {
+				logger.removeHandler(fh);
+				fh.close();
+			}
+				
 		}
-		
+
 		try {
 			File knowledgeFile = new File(Knowledge.KNOWLEDGE_FILE_NAME);
-			Files.move(knowledgeFile.getAbsoluteFile().toPath(), new File(fixedModelFolderName + "/" + knowledgeFile.getName()).toPath());
+			Files.move(knowledgeFile.getAbsoluteFile().toPath(),
+					new File(fixedModelFolderName + "/" + knowledgeFile.getName()).toPath());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * The selection strategy for the given experiment.
 	 * 
