8bac23fd3a89bcaecdbc9e321c97d62dc5e4641c
Magnus Marthinsen <magmar@online.no>
2020-04-11 17:14:06 +0200

    added scripts that is used to copy broken models to a seperate folder and scripts for reading metrics log

---
+++ src/hvl/projectparmorel/scripts/logreaders/MetricsLogReader.java
@@ -0,0 +1,402 @@
+package hvl.projectparmorel.scripts.logreaders;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+/**
+ * A script that can read the metrics log and extract the necasary information.
+ * This is returned as a csv.
+ * 
+ * @author Magnus
+ *
+ */
+public class MetricsLogReader {
+	private static String beforeEvaluationString = "---------------------------------------------------\n";
+	private static String afterEvaluationString = "---------------------------------------------------\n";
+
+	private static final JFileChooser fc = new JFileChooser();
+
+	public static void main(String[] args) {
+		System.out.println("Getting log file...");
+		File file = getLogFile();
+		if (file != null) {
+			if (file.getName().endsWith(".txt")) {
+				System.out.println("Intepreting log...");
+				List<ModelEvaluation> evaluations = interpretLog(file);
+				System.out.println("Generating csv...");
+				String csvString = generateCSVStringFrom(evaluations);
+				System.out.println("Getting destination file...");
+				File saveFile = getDestinationFile();
+				if (saveFile != null) {
+					System.out.println("Saving...");
+					writeTo(csvString, saveFile);
+					System.out.println("Finished!");
+				}
+			} else {
+				System.out.println("ERROR: Unsupported file format.");
+			}
+		}
+		System.exit(0);
+	}
+
+	/**
+	 * Gets the log file to read
+	 * 
+	 * @return the file to lread
+	 */
+	private static File getLogFile() {
+		int returnVal = fc.showOpenDialog(null);
+
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			return fc.getSelectedFile();
+		} else {
+			System.out.println("Cancelled by user.");
+			return null;
+		}
+	}
+
+	/**
+	 * Interprets the logs content and returns it as a list of model evaluations.
+	 * 
+	 * @param logFile
+	 * @return a list of model evaluations
+	 */
+	private static List<ModelEvaluation> interpretLog(File logFile) {
+		List<ModelEvaluation> evaluations = new ArrayList<>();
+
+		String log = readLog(logFile);
+		if (log != null) {
+			Pattern pattern = Pattern.compile(beforeEvaluationString + "(.*?)" + afterEvaluationString, Pattern.DOTALL);
+			Matcher matcher = pattern.matcher(log);
+
+			while (matcher.find()) {
+				String modelEvaluationString = matcher.group(1);
+				ModelEvaluation evaluation = new ModelEvaluation();
+				evaluation.setName(findNameForModel(modelEvaluationString));
+				evaluation.setNumberOfMetaclasses(findNumberOfMetaclassesFrom(modelEvaluationString));
+				evaluation.setNumberOfReferences(findNumberOfReferencesFrom(modelEvaluationString));
+				evaluation.setNumberOfAttributes(findNumberOfAttributesFrom(modelEvaluationString));
+				evaluation.setDit(findDITFrom(modelEvaluationString));
+				evaluation.setHagg(findHAggFrom(modelEvaluationString));
+				evaluation.setUnidirectionalRef(findNumberOfUnidirectionalRefFrom(modelEvaluationString));
+				evaluation.setOppositeRef(findNumberOfOppositeRefFrom(modelEvaluationString));
+				evaluation.setMaintainability(findMaintainabilityFrom(modelEvaluationString));
+				evaluation.setUnderstandability(findUnderstandabilityFrom(modelEvaluationString));
+				evaluation.setComplexity(findComplexityFrom(modelEvaluationString));
+				evaluation.setMetamodelReuse(findReuseFrom(modelEvaluationString));
+				evaluation.setRelaxationIndexOfMetamodel(findRelaxationIndexFrom(modelEvaluationString));
+
+				evaluations.add(evaluation);
+			}
+		}
+
+		return evaluations;
+	}
+
+	/**
+	 * Reads the log file and returns the content as a String
+	 * 
+	 * @param logFile
+	 * @return content as String
+	 */
+	private static String readLog(File logFile) {
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(logFile));
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
+			return sb.toString();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Finds a name for the model
+	 * 
+	 * @param modelEvaluationString
+	 * @return the name
+	 */
+	private static String findNameForModel(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("Analyzing model: " + "(.*?)" + "\n", Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			return matcher.group(1);
+		}
+		return null;
+	}
+
+	/**
+	 * Finds the number of metaclasses
+	 * 
+	 * @param modelEvaluationString
+	 * @return the number of metaclasses
+	 */
+	private static Integer findNumberOfMetaclassesFrom(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("Number of metaclasses: " + "(.*?)" + "\n", Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			String number = matcher.group(1);
+			return Integer.parseInt(number);
+		}
+		return null;
+	}
+
+	/**
+	 * Finds the number of references
+	 * 
+	 * @param modelEvaluationString
+	 * @return the number of references
+	 */
+	private static Integer findNumberOfReferencesFrom(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("Number of references: " + "(.*?)" + "\n", Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			String number = matcher.group(1);
+			return Integer.parseInt(number);
+		}
+		return null;
+	}
+
+	/**
+	 * Finds the number of attributes
+	 * 
+	 * @param modelEvaluationString
+	 * @return the number of attributes
+	 */
+	private static Integer findNumberOfAttributesFrom(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("Number of attributes: " + "(.*?)" + "\n", Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			String number = matcher.group(1);
+			return Integer.parseInt(number);
+		}
+		return null;
+	}
+
+	/**
+	 * Finds the DIT
+	 * 
+	 * @param modelEvaluationString
+	 * @return the DIT
+	 */
+	private static Integer findDITFrom(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("DIT: " + "(.*?)" + "\n", Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			String DIT = matcher.group(1);
+			return Integer.parseInt(DIT);
+		}
+		return null;
+	}
+
+	/**
+	 * Finds the HAgg
+	 * 
+	 * @param modelEvaluationString
+	 * @return the HAgg:
+	 */
+	private static Integer findHAggFrom(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("HAgg: " + "(.*?)" + "\n", Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			String HAgg = matcher.group(1);
+			return Integer.parseInt(HAgg);
+		}
+		return null;
+	}
+
+	/**
+	 * Finds the number of unidirectional references
+	 * 
+	 * @param modelEvaluationString
+	 * @return the number of unidirectional references
+	 */
+	private static Integer findNumberOfUnidirectionalRefFrom(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("Unidirectional Ref: " + "(.*?)" + " ", Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			String number = matcher.group(1);
+			return Integer.parseInt(number);
+		}
+		return null;
+	}
+
+	/**
+	 * Finds the number of opposite references
+	 * 
+	 * @param modelEvaluationString
+	 * @return the number of opposite references
+	 */
+	private static Integer findNumberOfOppositeRefFrom(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("Opposite refs: " + "(.*?)" + "\n", Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			String number = matcher.group(1);
+			return Integer.parseInt(number);
+		}
+		return null;
+	}
+
+	/**
+	 * Finds the maintainability
+	 * 
+	 * @param modelEvaluationString
+	 * @return the maintainability
+	 */
+	private static Double findMaintainabilityFrom(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("Evaluation of Maintainability of the metamodel: " + "(.*?)" + "\n",
+				Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			String number = matcher.group(1);
+			return Double.parseDouble(number);
+		}
+		return null;
+	}
+
+	/**
+	 * Finds the understandability
+	 * 
+	 * @param modelEvaluationString
+	 * @return the understandability
+	 */
+	private static Double findUnderstandabilityFrom(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("Evaluation of Understandability of the metamodel: " + "(.*?)" + "\n",
+				Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			String number = matcher.group(1);
+			return Double.parseDouble(number);
+		}
+		return null;
+	}
+
+	/**
+	 * Finds the complexity
+	 * 
+	 * @param modelEvaluationString
+	 * @return the complexity
+	 */
+	private static Double findComplexityFrom(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("Evaluation of Complexity of the metamodel: " + "(.*?)" + "\n",
+				Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			String number = matcher.group(1);
+			return Double.parseDouble(number);
+		}
+		return null;
+	}
+
+	/**
+	 * Finds the reuse
+	 * 
+	 * @param modelEvaluationString
+	 * @return the reuse
+	 */
+	private static Double findReuseFrom(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("Evaluation of Reuse of the metamodel: " + "(.*?)" + "\n", Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			String number = matcher.group(1);
+			return Double.parseDouble(number);
+		}
+		return null;
+	}
+
+	/**
+	 * Finds the Relaxation Index
+	 * 
+	 * @param modelEvaluationString
+	 * @return the Relaxation Index
+	 */
+	private static Double findRelaxationIndexFrom(String modelEvaluationString) {
+		Pattern pattern = Pattern.compile("Evaluation of Relaxation Index of the metamodel: " + "(.*?)" + "\n",
+				Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(modelEvaluationString);
+		while (matcher.find()) {
+			String number = matcher.group(1);
+			return Double.parseDouble(number);
+		}
+		return null;
+	}
+
+	/**
+	 * Generates a CSV-string from the evaluations.
+	 * 
+	 * @param evaluations
+	 * @return a string in CSV-format
+	 */
+	private static String generateCSVStringFrom(List<ModelEvaluation> evaluations) {
+		String csvString = "";
+		for (ModelEvaluation e : evaluations) {
+			csvString += e.toCsvString() + "\n";
+		}
+		return csvString;
+	}
+
+	/**
+	 * Writes content to file
+	 * 
+	 * @param content
+	 * @param file
+	 */
+	private static void writeTo(String content, File file) {
+		try {
+			file.createNewFile();
+			FileWriter myWriter = new FileWriter(file);
+			myWriter.write(ModelEvaluation.CSV_COLUMN_HEADER + "\n");
+			myWriter.write(content);
+			myWriter.close();
+			System.out.println("Successfully wrote to the file.");
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+		}
+	}
+
+	/**
+	 * Gets the destination file to save the output to.
+	 * 
+	 * @return the file to save to
+	 */
+	private static File getDestinationFile() {
+		JFrame parentFrame = new JFrame();
+		fc.setDialogTitle("Specify a .csv-file to save the result to");
+		int returnVal = fc.showSaveDialog(parentFrame);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			return fc.getSelectedFile();
+		} else {
+			System.out.println("Cancelled by user.");
+			return null;
+		}
+	}
+}
---
+++ src/hvl/projectparmorel/scripts/logreaders/ModelEvaluation.java
@@ -0,0 +1,149 @@
+package hvl.projectparmorel.scripts.logreaders;
+
+public class ModelEvaluation {
+	public static char CSV_SEPARATION_SIGN = ';';
+	public final static String CSV_COLUMN_HEADER = "Filename" + CSV_SEPARATION_SIGN + "Unidirectional refs"
+			+ CSV_SEPARATION_SIGN + "Opposite refs" + CSV_SEPARATION_SIGN + "Number of metaclasses"
+			+ CSV_SEPARATION_SIGN + "Number of references" + CSV_SEPARATION_SIGN + "Number of attributes"
+			+ CSV_SEPARATION_SIGN + "DIT" + CSV_SEPARATION_SIGN + "HAgg" + CSV_SEPARATION_SIGN + "Maintainability"
+			+ CSV_SEPARATION_SIGN + "Understantability" + CSV_SEPARATION_SIGN + "Complexity" + CSV_SEPARATION_SIGN
+			+ "Reuse of the metamodel" + CSV_SEPARATION_SIGN + "Relaxation index of the metamodel"
+			+ CSV_SEPARATION_SIGN;
+
+	private String name;
+	private Integer unidirectionalRef;
+	private Integer oppositeRef;
+	private Integer numberOfMetaclasses;
+	private Integer numberOfReferences;
+	private Integer numberOfAttributes;
+	private Integer dit;
+	private Integer hagg;
+	private Double maintainability;
+	private Double understandability;
+	private Double complexity;
+	private Double metamodelReuse;
+	private Double relaxationIndexOfMetamodel;
+
+	public ModelEvaluation() {
+	}
+
+	/**
+	 * Puts the variable on a CSV-format
+	 * 
+	 * @return the evaluation on a CSV-string
+	 */
+	public String toCsvString() {
+		String csvString = name + CSV_SEPARATION_SIGN + unidirectionalRef + CSV_SEPARATION_SIGN + oppositeRef + CSV_SEPARATION_SIGN
+				+ numberOfMetaclasses + CSV_SEPARATION_SIGN + numberOfReferences + CSV_SEPARATION_SIGN
+				+ numberOfAttributes + CSV_SEPARATION_SIGN + dit + CSV_SEPARATION_SIGN + hagg + CSV_SEPARATION_SIGN
+				+ maintainability + CSV_SEPARATION_SIGN + understandability + CSV_SEPARATION_SIGN + complexity
+				+ CSV_SEPARATION_SIGN + metamodelReuse + CSV_SEPARATION_SIGN + relaxationIndexOfMetamodel
+				+ CSV_SEPARATION_SIGN;
+		return csvString.replace("null", "");
+	}
+
+	public Integer getNumberOfMetaclasses() {
+		return numberOfMetaclasses;
+	}
+
+	public void setNumberOfMetaclasses(Integer numberOfMetaclasses) {
+		this.numberOfMetaclasses = numberOfMetaclasses;
+	}
+
+	public Integer getNumberOfReferences() {
+		return numberOfReferences;
+	}
+
+	public void setNumberOfReferences(Integer numberOfReferences) {
+		this.numberOfReferences = numberOfReferences;
+	}
+
+	public Integer getNumberOfAttributes() {
+		return numberOfAttributes;
+	}
+
+	public void setNumberOfAttributes(Integer numberOfAttributes) {
+		this.numberOfAttributes = numberOfAttributes;
+	}
+
+	public Integer getDit() {
+		return dit;
+	}
+
+	public void setDit(Integer dit) {
+		this.dit = dit;
+	}
+
+	public Integer getHagg() {
+		return hagg;
+	}
+
+	public void setHagg(Integer hagg) {
+		this.hagg = hagg;
+	}
+
+	public Double getMaintainability() {
+		return maintainability;
+	}
+
+	public void setMaintainability(Double maintainability) {
+		this.maintainability = maintainability;
+	}
+
+	public Double getUnderstandability() {
+		return understandability;
+	}
+
+	public void setUnderstandability(Double understandability) {
+		this.understandability = understandability;
+	}
+
+	public Double getComplexity() {
+		return complexity;
+	}
+
+	public void setComplexity(Double complexity) {
+		this.complexity = complexity;
+	}
+
+	public Double getMetamodelReuse() {
+		return metamodelReuse;
+	}
+
+	public void setMetamodelReuse(Double metamodelReuse) {
+		this.metamodelReuse = metamodelReuse;
+	}
+
+	public Double getRelaxationIndexOfMetamodel() {
+		return relaxationIndexOfMetamodel;
+	}
+
+	public void setRelaxationIndexOfMetamodel(Double relaxationIndexOfMetamodel) {
+		this.relaxationIndexOfMetamodel = relaxationIndexOfMetamodel;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getUnidirectionalRef() {
+		return unidirectionalRef;
+	}
+
+	public void setUnidirectionalRef(Integer unidirectionalRef) {
+		this.unidirectionalRef = unidirectionalRef;
+	}
+
+	public Integer getOppositeRef() {
+		return oppositeRef;
+	}
+
+	public void setOppositeRef(Integer oppositeRef) {
+		this.oppositeRef = oppositeRef;
+	}
+
+}
