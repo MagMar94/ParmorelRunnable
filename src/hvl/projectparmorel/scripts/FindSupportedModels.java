c9d359f615c18a14184b141efff53a45db32e873
Magnus Marthinsen <magmar@online.no>
2020-04-16 16:45:39 +0200

    added script that can figure out what models can be handled by parmorel

---
+++ src/hvl/projectparmorel/scripts/FindSupportedModels.java
@@ -0,0 +1,244 @@
+package hvl.projectparmorel.scripts;
+
+import java.awt.EventQueue;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import javax.swing.JFileChooser;
+
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+
+import hvl.projectparmorel.ecore.EcoreErrorExtractor;
+import hvl.projectparmorel.ecore.EcoreModel;
+import hvl.projectparmorel.ecore.EcoreModelProcessor;
+import hvl.projectparmorel.general.Error;
+import hvl.projectparmorel.general.ErrorExtractor;
+import hvl.projectparmorel.general.Model;
+import hvl.projectparmorel.general.ModelProcessor;
+import hvl.projectparmorel.general.ModelType;
+import hvl.projectparmorel.knowledge.Knowledge;
+
+public class FindSupportedModels {
+
+	private static Logger logger;
+	private static FileHandler fh;
+	private static JFileChooser fc;
+	private static List<File> supportedModels;
+	
+	public static void main(String[] args) {
+		fc = new JFileChooser();
+		supportedModels = new ArrayList<>();
+		List<Evaluation> evaluations = getTimeEvaluations(fc);
+		
+		File destinationFolder = getDestinationFolder(fc);
+		for (File f : supportedModels) {
+			try {
+				Files.copy(f.toPath(), new File(destinationFolder + "/" + f.getName()).toPath());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		File csvFile = new File(destinationFolder + "/0_overview.csv");
+		String csvString = generateCSVStringFrom(evaluations);
+		System.out.println("Saving...");
+		writeTo(csvString, csvFile);
+		System.out.println("Finished!");
+
+		if (fh != null) {
+			logger.removeHandler(fh);
+			fh.close();
+		}
+	}
+
+	/**
+	 * Promts the user for a folder containing all the results. It enters all the
+	 * subdirectories and reads the log, extracting model name and time.
+	 * 
+	 * @return of all the evaluatons
+	 */
+	public static List<Evaluation> getTimeEvaluations(JFileChooser fc) {
+		initializeLogger();
+
+		System.out.println("Getting model folder...");
+		File[] models = getModels(fc);
+
+		ErrorExtractor errorExtractor = new EcoreErrorExtractor();
+
+		ResourceSet resourceSet = new ResourceSetImpl();
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("ecore",
+				new EcoreResourceFactoryImpl());
+
+		Knowledge knowledge = new Knowledge();
+
+		List<Evaluation> evaluations = new ArrayList<>();
+		
+		for (File modelFile : models) {
+			URI uri = URI.createFileURI(modelFile.getAbsolutePath());
+			Resource modelResource = resourceSet.getResource(uri, true);
+			Model model = new EcoreModel(resourceSet, modelResource, uri);
+
+			List<Error> allErrors = errorExtractor.extractErrorsFrom(modelResource, true);
+			List<Error> unsupportedErrors = new ArrayList<>();
+			for(Error e : allErrors) {
+				if(ModelType.ECORE.doesNotSupportError(e.getCode())) {
+					unsupportedErrors.add(e);
+				}
+			}
+			allErrors.removeAll(unsupportedErrors);
+			
+			if (!allErrors.isEmpty()) {
+				ModelProcessor modelProcessor = new EcoreModelProcessor(knowledge);
+				Set<Integer> unsupportedErrorsCodes = modelProcessor.initializeQTableForErrorsInModel(model);
+				for (Integer code : unsupportedErrorsCodes) {
+					model.getModelType().addUnsupportedErrorCode(code);
+				}
+
+				List<Error> supportedErrors = new ArrayList<>();
+				supportedErrors.addAll(allErrors);
+				for (Error error : supportedErrors) {
+					if (unsupportedErrorsCodes.contains(error.getCode())) {
+						unsupportedErrors.add(error);
+					}
+				}
+				supportedErrors.removeAll(unsupportedErrors);
+				if (unsupportedErrors.isEmpty()) {
+					supportedModels.add(modelFile);
+				}
+				evaluations.add(new ModelEval(modelFile.getName(), supportedErrors, unsupportedErrors));
+			} else {
+				evaluations.add(new ModelEval(modelFile.getName(), new ArrayList<>(), unsupportedErrors));
+			}
+
+		}
+		
+		return evaluations;
+}
+
+	private static void initializeLogger() {
+		logger = Logger.getLogger("SupportedModels");
+		try {
+			fh = new FileHandler("SupportedModels.log");
+			logger.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();
+			fh.setFormatter(formatter);
+		} catch (SecurityException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+	}
+
+	private static int returnVal;
+
+	private static File[] getModels(JFileChooser fc) {
+		fc.setDialogTitle("Select the models");
+		fc.setMultiSelectionEnabled(true);
+		fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+		try {
+			EventQueue.invokeAndWait(new Runnable() {
+				@Override
+				public void run() {
+					returnVal = fc.showOpenDialog(null);
+				}
+			});
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			return fc.getSelectedFiles();
+		} else {
+			System.out.println("Cancelled by user.");
+		}
+		return null;
+	}
+
+	/**
+	 * Opens a dialog that lets the user select the folder to store the mutated
+	 * models.
+	 * 
+	 * @return a folder to store the models in, or null if the selected file is not
+	 *         a model or operation is cancelled
+	 */
+	private static File getDestinationFolder(JFileChooser fc) {
+		System.out.println("Getting destination file...");
+		fc.setDialogTitle("Select the destination folder");
+		fc.setMultiSelectionEnabled(false);
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+		try {
+			EventQueue.invokeAndWait(new Runnable() {
+				@Override
+				public void run() {
+					returnVal = fc.showOpenDialog(null);
+				}
+			});
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = fc.getSelectedFile();
+			if (selectedFile.isDirectory()) {
+				return selectedFile;
+			}
+			System.out.println("Selected file was not a folder.");
+			return null;
+		} else {
+			System.out.println("Cancelled by user.");
+			return null;
+		}
+	}
+	
+	/**
+	 * Generates a CSV-string from the evaluations.
+	 * 
+	 * @param evaluations
+	 * @return a string in CSV-format
+	 */
+	protected static String generateCSVStringFrom(List<Evaluation> evaluations) {
+		String csvString = "";
+		for (Evaluation e : evaluations) {
+			csvString += e.toCsvString() + "\n";
+		}
+		return csvString;
+	}
+
+	/**
+	 * Writes content to file
+	 * 
+	 * @param content
+	 * @param file
+	 */
+	private static void writeTo(String content, File file) {
+		try {
+			file.createNewFile();
+			FileWriter myWriter = new FileWriter(file);
+			myWriter.write(ModelEval.CSV_HEADER + "\n");
+			myWriter.write(content);
+			myWriter.close();
+			System.out.println("Successfully wrote to the file.");
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+		}
+	}
+}
---
+++ src/hvl/projectparmorel/scripts/ModelEval.java
@@ -0,0 +1,40 @@
+package hvl.projectparmorel.scripts;
+
+import java.util.List;
+import hvl.projectparmorel.general.Error;
+
+class ModelEval implements Evaluation {
+	public final static String CSV_SEPARATION_SIGN = ";";
+	public final static String CSV_HEADER = "Name" + CSV_SEPARATION_SIGN + "Supported" + CSV_SEPARATION_SIGN
+			+ "Number of errors" + CSV_SEPARATION_SIGN + "Supported error codes" + CSV_SEPARATION_SIGN + "Unupported error codes" + CSV_SEPARATION_SIGN;
+	private String modelName;
+	private List<Error> supportedErrors;
+	private List<Error> unsupportedErrors;
+
+	ModelEval(String modelName, List<Error> supportedErrors, List<Error> unsupportedErrors) {
+		this.modelName = modelName;
+		this.supportedErrors = supportedErrors;
+		this.unsupportedErrors = unsupportedErrors;
+	}
+
+	@Override
+	public String getName() {
+		return modelName;
+	}
+
+	@Override
+	public String toCsvString() {
+		return modelName + CSV_SEPARATION_SIGN + unsupportedErrors.isEmpty() + CSV_SEPARATION_SIGN
+				+ supportedErrors.size() + CSV_SEPARATION_SIGN + getErrorCodesAsString(supportedErrors) + CSV_SEPARATION_SIGN + getErrorCodesAsString(unsupportedErrors) + CSV_SEPARATION_SIGN;
+	}
+	
+	private String getErrorCodesAsString(List<Error> errors) {
+		String errorList = "";
+		
+		for(Error e : errors) {
+			errorList += e.getCode() + " :: ";
+		}
+		
+		return errorList;
+	}
+}
